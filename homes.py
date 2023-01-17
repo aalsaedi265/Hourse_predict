cdb870c396e8589ce5ffbe6548f36f815608e52d
aalsaedi265 <aalsaedi265@gmail.com>
2023-01-17 08:33:36 -0600

    first

---
+++ homes.py
@@ -0,0 +1,8 @@
+import pandas as pd
+import numpy as np
+import matplotlib .pyplot as plt
+import seaborn as sns
+
+#delimter in my data set is comaa so i don't need coma in specirction
+data = pd.read_csv('Housing.csv')
+
